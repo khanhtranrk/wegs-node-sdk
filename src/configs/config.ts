dd5d70c9351259ced78770ccecaa51af64b65850
Khanh Tran <khanhtran.rk@gmail.com>
2024-10-16 15:46:39 +0700

    fix config (4)

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -4,6 +4,8 @@ interface IConfig {
     baseURL: string;
 }
 
+console.log(process.env.WEGS_SERVER_DOMAIN);
+
 const config: IConfig = {
     baseURL: process.env.WEGS_SERVER_DOMAIN || 'https://api.example.com',
 };
