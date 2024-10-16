38de5142c511934565fd81eb73cb27e807b887e7
Khanh Tran <khanhtran.rk@gmail.com>
2024-10-16 09:21:50 +0700

    define apis

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import { Notebook } from "./models";
-import { NotebookService } from "./services";
-
-let notebook: Notebook = {
-    name: 'Notebook 1',
-    description: 'Notebook 1 description',
-}
-
-NotebookService.listPage(1).then((notebooks) => {
-    console.log(notebooks);
-}).catch((error) => {
-    console.error(error);
-});
+export * from './configs';
+export * from './models';
+export * from './services';
